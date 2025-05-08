cda0d2be95d52c1b4fdff2f51414aefea1ff1a27
Fulloa08 <f.ulloa08@ufromail.cl>
2025-05-08 18:49:51 -0400

    Archivo Main con gestión de errores

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,28 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arreglo = {3, 6, -2, -5, 7, 3};
+        int maxProducto = mayorAdyacente(arreglo);
+        System.out.println("El mayor producto de números adyacentes es: " + maxProducto);
+    }
+
+    public static int mayorAdyacente(int[] arreglo) {
+        if (arreglo == null){
+            throw new IllegalArgumentException("El arreglo no puede ser null")
+        }
+        if (arreglo.length < 2) {
+            throw new IllegalArgumentException("El arreglo debe tener al menos dos elementos");
+        }
+
+        int maxProducto = arreglo[0] * arreglo[1];
+
+        for (int i = 1; i < arreglo.length - 1; i++) {
+            int producto = arreglo[i] * arreglo[i + 1];
+            if (producto > maxProducto) {
+                maxProducto = producto;
+            }
+        }
+        return maxProducto;
+    }
+}
